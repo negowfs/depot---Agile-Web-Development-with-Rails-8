f736313c7130c9c2e6cb05b5c0fb30a710f59aaf
Weverson <negowfs@gmail.com>
2025-12-26 17:27:10 -0300

    Catalog Display

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,21 +11,49 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
+  test "index displays product list" do
+    get products_url
+    assert_response :success
+
+    assert_select "h1", "Products"
+    assert_select "tr", minimum: 1
+  end
+
+  test "index displays product attributes" do
+    get products_url
+    assert_response :success
+
+    assert_select "h1, h2", @product.title
+    assert_select "div", /\$[,\d]+\.\d\d/
+  end
+
+  test "should get new product form" do
     get new_product_url
     assert_response :success
+  end
+
+  test "new product form has required fields" do
+    get new_product_url
+    assert_response :success
+
+    assert_select "form[action=?][method=?]", products_path, "post" do
+      assert_select "input[name=?]", "product[title]"
+      assert_select "textarea[name=?]", "product[description]"
+      assert_select "input[name=?]", "product[price]"
+      assert_select "input[type=file][name=?]", "product[image]"
+    end
   end
 
   test "should create product" do
     assert_difference("Product.count") do
       post products_url, params: {
         product: {
+          title: @title,
           description: @product.description,
-          image: file_fixture_upload("lorem.jpg", "image/jpeg"),
           price: @product.price,
-          title: @title
-          }
+          image: file_fixture_upload("lorem.jpg", "image/jpeg")
         }
+      }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -36,20 +64,28 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit form" do
     get edit_product_url(@product)
     assert_response :success
+  end
+
+  test "edit form submits to correct action" do
+    get edit_product_url(@product)
+    assert_response :success
+
+    assert_select "form[action=?]", product_path(@product)
   end
 
   test "should update product" do
     patch product_url(@product), params: {
       product: {
+        title: @title,
         description: @product.description,
-        image: file_fixture_upload("lorem.jpg", "image/jpeg"),
         price: @product.price,
-        title: @title
-        }
+        image: file_fixture_upload("lorem.jpg", "image/jpeg")
       }
+    }
+
     assert_redirected_to product_url(@product)
   end
 
